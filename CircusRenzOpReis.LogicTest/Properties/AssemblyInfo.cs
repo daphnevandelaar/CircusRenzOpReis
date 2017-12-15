d157d4eb784d20f1f661f3ee27b24e30e4390ff7
Unknown <daphnevandelaar@live.nl>
2017-12-15 09:00:38 +0100

    wpf

---
+++ CircusRenzOpReis.LogicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CircusRenzOpReis.LogicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CircusRenzOpReis.LogicTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19c59df6-a16e-44fc-8728-861e8197142e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CircusRenzOpReis.WPF/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using CircuzRenzOpReis.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CircusRenzOpReis.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Animal> animals = new List<Animal>();
+        Train train = new Train();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPutAnimals_Click(object sender, RoutedEventArgs e)
+        {
+            List<Animal> largeCarbs = new List<Animal>();
+            List<Animal> mediumCarbs = new List<Animal>();
+            List<Animal> smallCarbs = new List<Animal>();
+            List<Animal> largeHerbs = new List<Animal>();
+            List<Animal> mediumHerbs = new List<Animal>();
+            List<Animal> smallHerbs = new List<Animal>();
+
+            foreach (Animal animal in lboxInputAnimals.Items)
+            {
+                animals.Add(animal);
+            }
+
+            List<Wagon> wagons = train.Arrange(animals);
+
+            lbAmountWagons.Content = $"Amount of wagons: {wagons.Count}";
+
+            lbBigCarnivores.Content = $"Larg Carnivores: {animals.FindAll(a => a.Carnivore == true && a.Size == AnimalSize.Large).Count}";
+            lbMediumCarnivores.Content = $"Medium Carnivores: {animals.FindAll(a => a.Carnivore == true && a.Size == AnimalSize.Medium).Count}";
+            lbSmallCarnivores.Content = $"Small Carnivores: {animals.FindAll(a => a.Carnivore == true && a.Size == AnimalSize.Small).Count}";
+
+            lbBigHerbivores.Content = $"Large Herbivores: {animals.FindAll(a => a.Carnivore == false && a.Size == AnimalSize.Large).Count}";
+            lbMediumHerbs.Content = $"Medium Herbivores: {animals.FindAll(a => a.Carnivore == false && a.Size == AnimalSize.Medium).Count}";
+            lbSmallHerbs.Content = $"Small Herbivores: {animals.FindAll(a => a.Carnivore == false && a.Size == AnimalSize.Small).Count}";
+
+            lboxInputAnimals.Items.Clear();
+            animals.Clear();
+            wagons.Clear();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbHerb.IsChecked == true)
+                lboxInputAnimals.Items.Add(new Animal(false, (AnimalSize)Enum.Parse(typeof(AnimalSize), cmbSize.Text)));
+            else
+                lboxInputAnimals.Items.Add(new Animal(true, (AnimalSize)Enum.Parse(typeof(AnimalSize), cmbSize.Text)));
+        }
+    }
+}
